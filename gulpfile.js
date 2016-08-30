de0ff5fdf914947275f0013e33ffa30184577165
Ray Huang <leoman730@gmail.com>
2016-08-30 11:35:04 -0400

    Fix gulp-shell for clearcache, use shorthand for shell command execution

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-var gulp        = require('gulp'),
-browserSync = require('browser-sync'),
-sass        = require('gulp-sass'),
-prefix      = require('gulp-autoprefixer'),
-shell       = require('gulp-shell');
-util        = require('gulp-util');
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    sass = require('gulp-sass'),
+    prefix = require('gulp-autoprefixer'),
+    shell = require('gulp-shell');
+util = require('gulp-util');
 
 
-var drupal_root = '/git_repos/drupal';
+var drupal_root = '/Users/ray/git_repos/nyu-law/drupal';
 
 /**
-* Launch the Server
-*/
+ * Launch the Server
+ */
 gulp.task('browser-sync', ['sass'], function() {
     browserSync.init({
         // Change as required
@@ -27,48 +27,53 @@ gulp.task('browser-sync', ['sass'], function() {
 });
 
 /**
-* @task sass
-* Compile files from scss
-*/
-gulp.task('sass', function () {
+ * @task sass
+ * Compile files from scss
+ */
+gulp.task('sass', function() {
     return gulp.src('scss/styles.scss')
-    .pipe(sass())
-    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({stream:true}))
+        .pipe(sass())
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true
+        }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 /**
-* @task clearcache
-* Clear all caches
-*/
-gulp.task('clearcache', function() {
-    return shell.task([
-        'drush cc all'
-    ]);
-});
+ * @task clearcache
+ * Clear all caches
+ */
+gulp.task('clearcache', shell.task([
+    'drush cc less'
+], {
+    cwd: drupal_root,
+    verbose: true
+}));
 
 /**
-* @task reload
-* Refresh the page after clearing cache
-*/
-gulp.task('reload', ['clearcache'], function () {
+ * @task reload
+ * Refresh the page after clearing cache
+ */
+gulp.task('reload', ['clearcache'], function() {
     browserSync.reload();
 });
 
 /**
-* @task watch
-* Watch scss files for changes & recompile
-* Clear cache when Drupal related files are changed
-*/
-gulp.task('watch', function () {
+ * @task watch
+ * Watch scss files for changes & recompile
+ * Clear cache when Drupal related files are changed
+ */
+gulp.task('watch', function() {
     // gulp.watch(['scss/*.scss', 'scss/**/*.scss'], ['sass']);
     // gulp.watch('**/*.{php,inc,info}',['reload']);
-    gulp.watch(drupal_root+"/sites/all/themes/nyulaw/**/*",['reload'])
+    gulp.watch(drupal_root + "/sites/all/themes/nyulaw/**/*", ['reload'])
 });
 
 /**
-* Default task, running just `gulp` will
-* compile Sass files, launch BrowserSync & watch files.
-*/
+ * Default task, running just `gulp` will
+ * compile Sass files, launch BrowserSync & watch files.
+ */
 gulp.task('default', ['browser-sync', 'watch']);
