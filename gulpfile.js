39b4c5f2eba5ead5faf6044f47569f18872ea100
Ray Huang <leoman730@gmail.com>
2016-12-08 16:21:35 -0500

    Add task runner to sync code to pantheon

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ util = require('gulp-util');
 
 
 var drupal_root = '/Users/ray/git_repos/nyu-law/drupal';
+var pantheon_root = '/Users/ray/git_repos/nyu-law-pantheon';
+var rsync_options = "--archive --compress --progress --verbose --stats --exclude='.svn*' --exclude='gulp*' --exclude='node_modules*' --exclude='grunttask*' --exclude='.git' ";
 
 /**
  * Launch the Server
@@ -47,7 +49,7 @@ gulp.task('sass', function() {
  * Clear all caches
  */
 gulp.task('clearcache', shell.task([
-    'drush cc less'
+    'drush cc less', 'drush cc css-js'
 ], {
     cwd: drupal_root,
     verbose: true
@@ -77,3 +79,42 @@ gulp.task('watch', function() {
  * compile Sass files, launch BrowserSync & watch files.
  */
 gulp.task('default', ['browser-sync', 'watch']);
+
+
+/** Watch less files changed and invoke drush cc less */
+gulp.task('watch-css', function() {
+    gulp.watch(
+        [
+            drupal_root + "/sites/all/themes/nyulaw_responsive/less/*",
+            drupal_root + "/sites/all/themes/nyulaw_responsive/css/*"
+        ], ['clearcache', 'styleguide-kss']);
+});
+
+
+gulp.task('styleguide-kss', shell.task([
+    'rm -f ' + drupal_root + '/sites/all/themes/nyulaw_responsive/styleguide/css/style.css',
+    'lessc ' + drupal_root + '/sites/all/themes/nyulaw_responsive/less/style.less ' + drupal_root + '/sites/all/themes/nyulaw_responsive/styleguide/css/style.css',
+    'npm-run kss --config kss-config.json'
+]));
+
+/**
+ * Sync responsive theme to pantheon.
+ *
+ * This take care of git push and drush cc all.
+ *
+ * @type {[type]}
+ */
+gulp.task('pantheon-responsive-sync', shell.task([
+    'git checkout responsive',
+    'rsync ' + rsync_options + drupal_root + '/sites/all/themes/nyulaw_responsive/ ' + pantheon_root + '/sites/all/themes/nyulaw_responsive/',
+    'git commit -am "Sync from dev."',
+    'git push origin responsive',
+    'drush @pantheon.nyu-law-school.responsive cc all'
+
+], {
+    cwd: pantheon_root,
+    verbose: true,
+    env: {
+        PATH: process.env.PATH
+    }
+}));
