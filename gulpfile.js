b6d72f6f070df0d854fde3a88c51f172053a81e6
leoman730 <leoman730@gmail.com>
2018-03-08 13:02:19 -0500

    Update script for Drupal 8 development.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,18 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     prefix = require('gulp-autoprefixer'),
     shell = require('gulp-shell'),
-    util = require('gulp-util');
+    util = require('gulp-util'),
+    less = require('gulp-less'),
+    sourcemaps = require('gulp-sourcemaps');
 
 
-var drupal_root = '/Users/ray/git_repos/nyu-law/drupal';
+var LessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+
+var drupal_root = '/Users/huangr/git_repos/nyu-law-d8/web';
+var drupal_theme_path = drupal_root + '/themes/custom/nyulaw';
+
+// need change
 var pantheon_root = '/Users/ray/git_repos/nyu-law-pantheon';
 var rsync_options = "--archive --compress --progress --verbose --stats --exclude='.svn*' --exclude='gulp*' --exclude='node_modules*' --exclude='grunttask*' --exclude='.git' ";
 
@@ -49,7 +57,7 @@ gulp.task('sass', function() {
  * Clear all caches
  */
 gulp.task('clearcache', shell.task([
-    'drush cc less', 'drush cc css-js'
+    'fin drush cr'
 ], {
     cwd: drupal_root,
     verbose: true
@@ -71,7 +79,7 @@ gulp.task('reload', ['clearcache'], function() {
 gulp.task('watch', function() {
     // gulp.watch(['scss/*.scss', 'scss/**/*.scss'], ['sass']);
     // gulp.watch('**/*.{php,inc,info}',['reload']);
-    gulp.watch(drupal_root + "/sites/all/themes/nyulaw/**/*", ['reload']);
+    gulp.watch(drupal_root + "/themes/custom/nyulaw/less/*", ['reload']);
 });
 
 /**
@@ -82,19 +90,32 @@ gulp.task('default', ['browser-sync', 'watch']);
 
 
 /** Watch less files changed and invoke drush cc less */
-gulp.task('watch-css', function() {
+gulp.task('watch-less', function() {
     gulp.watch(
         [
-            drupal_root + "/sites/all/themes/nyulaw_responsive/less/*",
-            drupal_root + "/sites/all/themes/nyulaw_responsive/css/*"
-        ], ['clearcache', 'styleguide-kss']);
+          drupal_theme_path + "/less/*",
+        ], ['less', 'styleguide-kss','clearcache']);
 });
 
 
+
+
+gulp.task('less', function(){
+  return gulp.src(drupal_theme_path + '/less/style.less')
+    .pipe(sourcemaps.init())
+    .pipe(less({
+      plugins: [autoprefix]
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(drupal_theme_path + '/css'));
+
+});
+
+
+
 gulp.task('styleguide-kss', shell.task([
-    'rm -f ' + drupal_root + '/sites/all/themes/nyulaw_responsive/styleguide/css/style.css',
-    'lessc ' + drupal_root + '/sites/all/themes/nyulaw_responsive/less/style.less ' + drupal_root + '/sites/all/themes/nyulaw_responsive/styleguide/css/style.css',
-    'npm-run kss --config kss-config.json'
+  'rm -f ' + drupal_theme_path + '/styleguide/css/style.css',
+  'npm-run kss --config kss-config.json'
 ]));
 
 /**
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 Some simple task runner scripts for Drupal development.
+
+```
+# only if you want gulp to be install globally
+sudo npm install gulp-cli -g
+
+# then
+npm install
+```
